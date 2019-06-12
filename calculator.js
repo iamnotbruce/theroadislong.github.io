3633fb3be5935a0f672dc5ae91b2d2b92bc5c410
iamnotbruce <leetong.b@gmail.com>
2019-06-12 08:33:00 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,3 +11,17 @@ var g = $('#G').slider()
 var b = $('#B').slider()
 		.on('slide', RGBChange)
 		.data('slider');
+
+var slider1 = document.getElementById("load");
+var slider2 = document.getElementById("march");
+var output1 = document.getElementById("weight");
+var output2 = document.getElementById("speed");
+output1.innerHTML = slider1.value;
+output2.innerHTML = slider2.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
