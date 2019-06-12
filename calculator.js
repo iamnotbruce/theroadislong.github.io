0bd21739b5e03db59c179e80d1bf74a5553f0971
iamnotbruce <leetong.b@gmail.com>
2019-06-12 18:49:47 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -35,9 +35,9 @@ function calculate(sval1,sval2) {
   var weight = 60;
   var terrain = 1;
   var grade = 0;
-  var pandolf = 1.5*weight+2*(weight+sval1)*(sval1/weight)^2+terrain*(weight+sval1)*(1.5*(sval2/3.6)^2+0.35*(sval2/3.6)*grade)
+  var pandolf = 1.5*weight+2*(weight+sval1)*(sval1/weight)*(sval1/weight)+terrain*(weight+sval1)*(1.5*(sval2/3.6)*(sval2/3.6)+0.35*(sval2/3.6)*grade)
   	width =((((pandolf*0.0143)/5.05)/weight)*1000)/3.5
 	width = Math.round(width * 100) / 100
-	elem.style.width = width * 10;
+	elem.width = width * 10;
 	elem.innerHTML = width * 1 + ' MET';
 }
