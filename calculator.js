eeb61defd1da0c4a10f06e5cdd79566d5e351456
iamnotbruce <leetong.b@gmail.com>
2019-06-13 09:32:58 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -30,12 +30,12 @@ slider2.oninput = function() {
 }
 
 function calculate() {
-  var elem = document.getElementById("metpBar");
-  var load = document.getElementById("load").value;
-  var speed = document.getElementById("march").value;
-  var bm = 60;
-  var terrain = 1;
-  var grade = 0;
+	var elem = document.getElementById("metpBar");
+	var load = document.getElementById("load").value;
+	var speed = document.getElementById("march").value;
+	var bm = 60;
+	var terrain = 1;
+	var grade = 0;
   // pandolf=1.5*B3+2*(B3+C3)*(C3/B3)^2+D3*(B3+C3)*(1.5*E3^2+0.35*E3*F3)
   // met=((((AG3*0.0143)/5.05)/B3)*1000)/3.5
   // b = bm(kg)
@@ -43,9 +43,9 @@ function calculate() {
   // d = terrain
   // e = speed(m/s)
   // f = grade(%)
-  var pandolf = 1.5*bm + 2*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
-  var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5;
-  var width = Math.round(met * 100) / 100;
-	elem.width = width;
-	elem.innerHTML = width + ' MET';
+	var pandolf = 1.5*bm + 2*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
+	var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5;
+	var width = Math.round(met * 100) / 100;
+	document.getElementById("metpBar").style.width = width + "%";
+	elem.innerHTML = width + " MET";
 }
