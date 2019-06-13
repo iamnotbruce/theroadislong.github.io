0e9da57442b0275c965077b368457f81bfd98229
iamnotbruce <leetong.b@gmail.com>
2019-06-13 11:35:43 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,24 +7,28 @@ output2.innerHTML = slider2.value;
 
 slider1.oninput = function() {
   output1.innerHTML = this.value;
-  calculateMET();
+  updateMetpbar();
 }
 slider2.oninput = function() {
   output2.innerHTML = this.value;
-  calculateMET();
+  updateMetpbar();
 }
 
-function calculateMET() {
-	var elem = document.getElementById("metpBar");
+function calculateMET(bm) {
 	var load = document.getElementById("load").value;
 	var speed = document.getElementById("march").value;
-	var bm = 66.5;
 	var terrain = 1.0;
 	var grade = 0;
 	load = Number(load);
 	speed = Number(speed);
 	var pandolf = 1.5*bm + 2.0*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
 	var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5;
+	return met;
+}
+
+function updateMetpbar() {
+	var elem = document.getElementById("metpBar");
+	var met = calculateMet(66.5);
 	var width = Math.round(met * 100) / 100;
 	if (width > 10){
 		elem.style.width = "100%";
@@ -34,43 +38,47 @@ function calculateMET() {
 	elem.innerHTML = width + " MET";
 }
 
-
-function plot() {
-	
+function tte(wt){
+	//var metp = 
+	var wtt = Math.exp(9-0.8*metp);
+	wtt = Math.floor(wtt);
+	return wtt;
 }
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['50kg', '60kg', '70kg', '80kg', '90kg'],
-        datasets: [{
-            label: 'Body Weight',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-		'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-		'rgba(54, 162, 235, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-		'rgba(255, 159, 64, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-		'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+function plot() {
+	var ctx = document.getElementById('myChart');
+	var myChart = new Chart(ctx, {
+    	type: 'bar',
+    	data: {
+        	labels: ['50kg', '60kg', '70kg', '80kg', '90kg'],
+        	datasets: [{
+            	label: 'Body Weight',
+            	data: [50, 60, 70, 80, 90],
+            	backgroundColor: [
+                	'rgba(255, 99, 132, 0.2)',
+			'rgba(255, 159, 64, 0.2)',
+                	'rgba(255, 206, 86, 0.2)',
+                	'rgba(75, 192, 192, 0.2)',
+			'rgba(54, 162, 235, 0.2)'
+            	],
+            	borderColor: [
+                	'rgba(255, 99, 132, 1)',
+			'rgba(255, 159, 64, 1)',
+                	'rgba(255, 206, 86, 1)',
+                	'rgba(75, 192, 192, 1)',
+			'rgba(54, 162, 235, 1)'
+            	],
+            	borderWidth: 1
+        	}]
+    	},
+    	options: {
+        	scales: {
+            	yAxes: [{
+                	ticks: {
+                    	beginAtZero: true
+                	}
+            	}]
+        	}
+    	}
+	});
+}
