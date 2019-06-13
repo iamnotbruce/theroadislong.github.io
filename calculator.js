a1686945266bfcf1ed51e06b571a97a575bf5da4
iamnotbruce <leetong.b@gmail.com>
2019-06-13 08:38:26 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -44,10 +44,10 @@ function calculate() {
   // d = terrain
   // e = speed(m/s)
   // f = grade(%)
-  //var pandolf = 1.5*bm+2*(bm+load)*(load/bm)*(load/bm)+terrain*(bm+load)*(1.5*(speed/3.6)*(speed/3.6)+0.35*(speed/3.6)*grade)
+  var pandolf = 1.5*bm+2*(bm+load)*(load/bm)*(load/bm)+terrain*(bm+load)*(1.5*(speed/3.6)*(speed/3.6)+0.35*(speed/3.6)*grade)
   var pandolf = 10.678
   var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5
-	width = Math.round(load * 100) / 100
+	width = Math.round(speed * 100) / 100
 	elem.width = width;
 	elem.innerHTML = width + ' MET';
 }
