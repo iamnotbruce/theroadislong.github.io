2a3e92b9bfcd4e2748963c0bd9a455ec81a1e53f
iamnotbruce <leetong.b@gmail.com>
2019-06-13 09:53:41 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -33,8 +33,8 @@ function calculate() {
 	var elem = document.getElementById("metpBar");
 	var load = document.getElementById("load").value;
 	var speed = document.getElementById("march").value;
-	var bm = 60;
-	var terrain = 1;
+	var bm = 60.0;
+	var terrain = 1.0;
 	var grade = 0;
   // pandolf=1.5*B3+2*(B3+C3)*(C3/B3)^2+D3*(B3+C3)*(1.5*E3^2+0.35*E3*F3)
   // met=((((AG3*0.0143)/5.05)/B3)*1000)/3.5
@@ -43,7 +43,7 @@ function calculate() {
   // d = terrain
   // e = speed(m/s)
   // f = grade(%)
-	var pandolf = 1.5*bm + 2*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
+	var pandolf = 1.5*bm + 2.0*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
 	var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5;
 	var width = Math.round(met * 100) / 100;
 	elem.style.width = width + "%";
