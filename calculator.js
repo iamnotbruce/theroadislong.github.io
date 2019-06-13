7671e6a77361ae8b3e9b49a6e74c9b93dcb8fab2
iamnotbruce <leetong.b@gmail.com>
2019-06-13 10:14:20 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,18 +1,3 @@
-/*
-var RGBChange = function() {
-	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-};
-
-var r = $('#R').slider()
-		.on('slide', RGBChange)
-		.data('slider');
-var g = $('#G').slider()
-		.on('slide', RGBChange)
-		.data('slider');
-var b = $('#B').slider()
-		.on('slide', RGBChange)
-		.data('slider');
-*/
 var slider1 = document.getElementById("load");
 var slider2 = document.getElementById("march");
 var output1 = document.getElementById("weight");
@@ -55,3 +40,41 @@ function calculate() {
 	}
 	elem.innerHTML = width + " MET";
 }
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
