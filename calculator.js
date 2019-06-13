1a8810213a8e0e0bc780e9545a35d3f5e8e0d81f
iamnotbruce <leetong.b@gmail.com>
2019-06-13 14:41:44 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,20 +1,22 @@
 var slider1 = document.getElementById("load");
-var slider2 = document.getElementById("march");
 var output1 = document.getElementById("weight");
-var output2 = document.getElementById("speed");
 output1.innerHTML = slider1.value;
-output2.innerHTML = slider2.value;
-
 slider1.oninput = function() {
   	output1.innerHTML = this.value;
   	updateMetpbar();
 	plot();
 }
+
+/*
+var slider2 = document.getElementById("march");
+var output2 = document.getElementById("speed");
+output2.innerHTML = slider2.value;
 slider2.oninput = function() {
   	output2.innerHTML = this.value;
   	updateMetpbar();
   	plot();
 }
+*/
 
 function calculateMET(bm) {
 	var load = document.getElementById("load").value;
