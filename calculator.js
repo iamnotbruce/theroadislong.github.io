80d7e53fc1829354b0e106e27936c66c824b546b
iamnotbruce <leetong.b@gmail.com>
2019-06-13 09:35:53 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -46,6 +46,6 @@ function calculate() {
 	var pandolf = 1.5*bm + 2*(bm+load)*(load/bm)*(load/bm) + terrain*(bm+load)* 1.5*(speed/3.6)*(speed/3.6) + 0.35*(speed/3.6)*grade;
 	var met =((((pandolf*0.0143)/5.05)/bm)*1000)/3.5;
 	var width = Math.round(met * 100) / 100;
-	document.getElementById("metpBar").style.width = width + "%";
+	elem.style.width = width + "%";
 	elem.innerHTML = width + " MET";
 }
